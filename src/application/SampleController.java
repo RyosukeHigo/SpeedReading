69dd55b4fae9dbec847c58c1bc17f5b4c1ce43dd
ryosuke <ryosuke@DESKTOP-4LDF9AD>
2016-11-04 05:50:05 +0900

    some change

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,10 +1,17 @@
 package application;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import org.atilika.kuromoji.Token;
-import org.atilika.kuromoji.Tokenizer;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+//import com.sun.javafx.fxml.expression.Expression.Parser.Token;
 
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
@@ -17,36 +24,36 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 public class SampleController implements Initializable{
 	@FXML private Button Button1;
-    @FXML private Label Label1;
-    @FXML private Slider Slider1;
-    @FXML private TextArea TextArea1;
-    @FXML private Label Speed;
-    @FXML private Button test;
-    @FXML private Label MinLength;
-    @FXML private Button plus;
-    @FXML private Button minus;
-    private Display d;
+	@FXML private Label Label1;
+	@FXML private Slider Slider1;
+	@FXML private TextArea TextArea1;
+	@FXML private Label Speed;
+	@FXML private Button test;
+	@FXML private Label MinLength;
+	@FXML private Button plus;
+	@FXML private Button minus;
+	private Display d;
 
-    @FXML
-    public void onClicked(ActionEvent event){
-		d.setString(TextArea1.getText());
+	@FXML
+	public void onClicked(ActionEvent event){
+		d.setString(TextArea1.getText().replaceAll("\n", ""));
 		d.setLabel(Label1);
 		d.setSlider(Slider1);
 		d.setMin(MinLength);
 		d.restart();
-    }
-    @FXML
-    public void onDragDetected() {
-    	Speed.setText(Integer.valueOf(MinLength.getText())*(60*1000)/(650-(int)Slider1.getValue()*6)+"Char/s");
 	}
-    @FXML
-    public void onClikedPlus(){
-    	MinLength.setText(String.valueOf(Integer.valueOf(MinLength.getText())+1));
-    }
-    @FXML
-    public void onClikedMinus(){
-    	MinLength.setText(String.valueOf(Integer.valueOf(MinLength.getText())-1));
-    }
+	@FXML
+	public void onDragDetected() {
+		Speed.setText(Integer.valueOf(MinLength.getText())*(60*1000)/(650-(int)Slider1.getValue()*6)+"Char/s");
+	}
+	@FXML
+	public void onClikedPlus(){
+		MinLength.setText(String.valueOf(Integer.valueOf(MinLength.getText())+1));
+	}
+	@FXML
+	public void onClikedMinus(){
+		MinLength.setText(String.valueOf(Integer.valueOf(MinLength.getText())-1));
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -72,33 +79,49 @@ public class SampleController implements Initializable{
 		private void setMin(Label min){
 			this.min = min;
 		}
-	    protected Task<Void> createTask() {
-
-	        return new Task<Void>() {
-	            protected Void call() {
-	    			Tokenizer tokenizer = Tokenizer.builder().build();
-	    			String text = s.replaceAll("\n", "");
-	    			List<Token> tokens = tokenizer.tokenize(text);
-	    			tmp = "";
-	    			for (Token token : tokens) {
-	    				tmp = tmp + token.getSurfaceForm();
-	    				if(tmp.length()>=Integer.valueOf(min.getText())){
-	    					//Platform.runLater( () -> l.setText(tmp));
-	    					//l.setText(tmp);
-	    					updateMessage(tmp);
-	    					tmp = "";
-	    				}else{
-	    					continue;
-	    				}
-	    				try{
-	    					Thread.sleep((int)(650 - sld.getValue()*6));
-	    					//System.out.println(sld.getValue());
-	    				}catch(InterruptedException e){};
-	    			}
-	    			updateMessage(tmp);
+		protected Task<Void> createTask() {
+			return new Task<Void>() {
+				protected Void call() {
+					YahooAPI API = new YahooAPI(s);
+					try {
+						API.textAPI();
+					} catch (IOException e2) {
+						// TODO 自動生成された catch ブロック
+						e2.printStackTrace();
+					}
+					SAXParserFactory spfactory = SAXParserFactory.newInstance();
+					SAXParser parser = null;
+					try {
+						parser = spfactory.newSAXParser();
+					} catch (ParserConfigurationException | SAXException e1) {
+						// TODO 自動生成された catch ブロック
+						e1.printStackTrace();
+					}
+					SaxSample sax = new SaxSample();
+					try {
+						parser.parse(new File("test.xml"), sax);
+					} catch (SAXException | IOException e1) {
+						// TODO 自動生成された catch ブロック
+						e1.printStackTrace();
+					}
+					System.out.println("ste");
+					final ArrayList<String> TextList = sax.getResult();
+					TextList.get(0);
+					for (int i=0;i<TextList.size();i++) {
+						tmp = TextList.get(i);
+						System.out.println(tmp);
+						updateMessage(tmp);
+							try {
+								Thread.sleep((int)(650 - sld.getValue()*6));
+							} catch (InterruptedException e) {
+								// TODO 自動生成された catch ブロック
+								e.printStackTrace();
+							}
+							//System.out.println(sld.getValue());
+					}
 					return null;
-	            }
-	        };
-	    }
+				}
+			};
+		}
 	}
 }
---
+++ src/application/YahooAPI.java
@@ -0,0 +1,80 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class YahooAPI {
+	static String input;
+	/**
+	 * Yahoo!ディベロッパーのAPP ID
+	 */
+	private static String APP_ID = "dj0zaiZpPXY3U0syYkg4eXR5cSZzPWNvbnN1bWVyc2VjcmV0Jng9MDg-";
+
+	/**
+	 * Yahooテキスト分析APIのベースURI
+	 */
+	private static String BASE_URI = "http://jlp.yahooapis.jp/DAService/V1/parse";
+	YahooAPI(String input){
+		YahooAPI.input = input;
+	}
+//	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException{
+//		Scanner sc = new Scanner(System.in);
+//		try{
+//			String input = sc.next();
+//			String utf8 = new String(input.getBytes("UTF8"), "UTF8");
+//			textAPI(utf8);
+//
+//		}catch(IOException e){
+//			e.printStackTrace();
+//		}
+//		SAXParserFactory spfactory = SAXParserFactory.newInstance();
+//
+//        SAXParser parser = spfactory.newSAXParser();
+//
+//        parser.parse(new File("test.xml"), new SaxSample());
+//	}
+
+	public void textAPI() throws IOException{
+		String utf8 = new String(input.getBytes("UTF8"), "UTF8");
+		if(utf8 != null){
+			utf8 = URLEncoder.encode(utf8,"UTF8");
+			URL url = new URL(BASE_URI+"?appid="+APP_ID+"&sentence="+utf8);
+			HttpURLConnection urlconn = (HttpURLConnection)url.openConnection();
+			urlconn.setRequestMethod("GET");
+			urlconn.setInstanceFollowRedirects(false);
+
+			urlconn.connect();
+
+			BufferedReader reader =
+					new BufferedReader(new InputStreamReader(urlconn.getInputStream()));
+
+			StringBuffer responseBuffer = new StringBuffer();
+			while (true){
+				String line = reader.readLine();
+				if ( line == null ){
+					break;
+				}
+				responseBuffer.append(line);
+			}
+
+			reader.close();
+			urlconn.disconnect();
+
+			String response = responseBuffer.toString();
+			//System.out.println(response);
+			File file = new File("test.xml");
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+			pw.print(response);
+			pw.close();
+		}
+	}
+
+}
