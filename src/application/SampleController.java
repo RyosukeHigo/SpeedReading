5d94fe3abfeb115110d156a82f61839c3bc54029
RyosukeHigo <rubik_cube_serisuza_oh@yahoo.co.jp>
2017-01-19 20:19:28 +0900

    UI changed

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -36,25 +36,27 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 public class SampleController implements Initializable{
 	@FXML private Button Button1;
 	@FXML private Label Label1;
 	@FXML private Slider Slider1;
+	@FXML private Slider Slider2;
 	@FXML private TextArea TextArea1;
 	@FXML private Label Speed;
+	@FXML private Label Speed2;
 	@FXML private Button test;
 	@FXML private Label MinLength;
 	@FXML private Button Random;
-	@FXML private Button slowish;
-	@FXML private Button slow;
-	@FXML private Button comfortable;
-	@FXML private Button fastish;
-	@FXML private Button fast;
 	@FXML private CheckBox proxyButton;
 	@FXML private CheckBox spaceButton;
+	@FXML private CheckBox constButton;;
 	@FXML private Button toNext;
 	@FXML private Label bNum;
+	@FXML private TextField aField;
+	@FXML private TextField bField;
+	@FXML private TextField dField;
 	private Display d;
 	private ArrayList<String> NewsList = new ArrayList<>();
 	File file = null;
@@ -84,33 +86,26 @@ public class SampleController implements Initializable{
 		//Button1.setDisable(true);
 		d.setLabel(Label1);
 		d.setSlider(Slider1);
+		d.set2Slider(Slider2);
 		d.setProxyBox(proxyButton);
 		d.setSpaceBox(spaceButton);
 		d.setbNum(bNum);
+		d.setaField(aField);
+		d.setbField(bField);
+		d.setdField(dField);
+		d.setConstBox(constButton);
 		d.restart();
 		toNext.requestFocus();
 	}
 	@FXML
 	public void onClickedValue(ActionEvent event){
-		if(event.getSource() == slow){
-			pw.print(",0\n");
-		}else if(event.getSource() == slowish){
-			pw.print(",1\n");
-		}else if(event.getSource() == comfortable){
-			pw.print(",2\n");
-		}else if(event.getSource() == fastish){
-			pw.print(",3\n");
-		}else if(event.getSource() == fast){
-			pw.print(",4\n");
-		}
-		pw.close();
-		//Random.setDisable(false);
-		//Button1.setDisable(false);
+
 	}
 
 	@FXML
 	public void onDragDetected() {
 		Speed.setText(String.valueOf((int)Slider1.getValue()));
+		//Speed2.setText(String.valueOf((int)Slider2.getValue()));
 	}
 	@FXML
 	public void onPressed() {
@@ -129,7 +124,7 @@ public class SampleController implements Initializable{
 		TextInputDialog userNameDialog  = new TextInputDialog( "InputYourName" );
 		userNameDialog.setTitle("Your Name");
 		userNameDialog.setHeaderText("Input your name!");
-	    String userName = userNameDialog.showAndWait().orElse("");
+		String userName = userNameDialog.showAndWait().orElse("");
 		String myName = myFormat.format(myCal.getTime()) + userName +".csv";
 		file = new File(myName);
 		try{
@@ -150,17 +145,21 @@ public class SampleController implements Initializable{
 		}
 		Label1.textProperty().bind(d.messageProperty());
 
-		//Speed.setText(String.valueOf((int)Slider1.getValue()));
-		spaceButton.setSelected(true);;
+		//Speed.setText(String.valueOf((int)Slider1.getValue()))
 	}
 	static class Display extends Service<Void> {
 		String inputText;
 		Label l;
-		Slider sld;
+		Slider sld1;
+		Slider sld2;
 		Label min;
 		CheckBox proxyBox;
 		CheckBox spaceBox;
+		CheckBox constBox;
 		Label bNum;
+		TextField aField;
+		TextField bField;
+		TextField dField;
 		private String displayText = "";
 		private void setString(String s) {
 			this.inputText = s;
@@ -169,7 +168,10 @@ public class SampleController implements Initializable{
 			this.l = l;
 		}
 		private void setSlider(Slider sld){
-			this.sld = sld;
+			this.sld1 = sld;
+		}
+		private void set2Slider(Slider sld){
+			this.sld2 = sld;
 		}
 		private void setProxyBox(CheckBox proxyBox){
 			this.proxyBox = proxyBox;
@@ -180,12 +182,34 @@ public class SampleController implements Initializable{
 		private void setbNum(Label bNum){
 			this.bNum = bNum;
 		}
+		public TextField getaField() {
+			return aField;
+		}
+		public void setaField(TextField aField) {
+			this.aField = aField;
+		}
+		public TextField getbField() {
+			return bField;
+		}
+		public void setbField(TextField bField) {
+			this.bField = bField;
+		}
+		public TextField getdField() {
+			return dField;
+		}
+		public void setdField(TextField dField) {
+			this.dField = dField;
+		}
+
+		public CheckBox getConstBox() {
+			return constBox;
+		}
+		public void setConstBox(CheckBox constBox) {
+			this.constBox = constBox;
+		}
 		protected Task<Void> createTask() {
 			return new Task<Void>() {
 				protected Void call() throws InterruptedException {
-					if(!spaceBox.isSelected()){
-						System.out.println("OK");
-					}
 					YahooAPI API = new YahooAPI(inputText,proxyBox.isSelected());
 					try {
 						API.textAPI("parse");
@@ -211,14 +235,10 @@ public class SampleController implements Initializable{
 					ArrayList<String> TextList = psax.getResult();
 					ArrayList<Integer> ScoreList = psax.getScore();
 					ArrayList<Integer> LengthList = psax.getTextReadLength();
-					java.util.Random rnd = new java.util.Random();
-					int lengthWeight = 10*rnd.nextInt(11);//0~100の10刻み
-					int scoreWeight = 10*rnd.nextInt(11);//0~50の5刻み
-					int baseTime = 50*rnd.nextInt(6);
-					if(spaceBox.isSelected()==false){
-						pw.print(baseTime+","+lengthWeight+","+scoreWeight);
-					}
-					int a=0;
+					double a = Double.valueOf(aField.getText());
+					double b = Double.valueOf(bField.getText());
+					int baseTime = (int) (Double.valueOf(dField.getText())-50*sld1.getValue());
+					if(baseTime<0) baseTime = 0;
 					updateMessage("■スペースキーを押してスタート■");
 					if(spaceBox.isSelected()==true){
 						while(true){
@@ -236,52 +256,74 @@ public class SampleController implements Initializable{
 					Thread.sleep(500);
 					updateMessage("1");
 					Thread.sleep(500);
-					for (int i=0;i<TextList.size();i++) {
-						displayText = TextList.get(i);
-						int kanjiNum = 0;
-						for(int k=0;k<displayText.length();k++){
-							 if(UnicodeBlock.of(displayText.charAt(k)) == UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS){
-								 kanjiNum++;
-							 }
-						}
-						long start = System.currentTimeMillis();
-
-						updateMessage(displayText);
-						int textLength = displayText.length();//LengthList.get(i);
-						int textScore = ScoreList.get(i);
-						System.out.println(displayText + " " + textLength + " " + textScore);
-						int displayTime = (int)(baseTime + lengthWeight*textLength + scoreWeight*textScore);
-						sumOfB++;
-						if(spaceBox.isSelected()==true){
-							while(true){
-								if(isNext){
-									isNext = false;
-									break;
-								}
-								Thread.sleep(1);
+					int displayTime = 0;
+					if(constBox.isSelected()){
+						int i=0;
+						while(true){
+							int lg = 5;
+							if((i+1)*lg<inputText.length()){
+								displayText = inputText.substring(lg*i, lg*(i+1));
+							}else{
+								displayText = inputText.substring(lg*i, inputText.length());
+								break;
 							}
-
-						}else{
+							displayTime = (int) (1050-50*(sld2.getValue()));
+							updateMessage(displayText);
 							try {
 								Thread.sleep(displayTime);
 							} catch (InterruptedException e) {
 								e.printStackTrace();
 							}
+							i++;
 						}
+					}else{
+						for (int i=0;i<TextList.size();i++) {
+							displayText = TextList.get(i);
+							int kanjiNum = 0;
+							for(int k=0;k<displayText.length();k++){
+								if(UnicodeBlock.of(displayText.charAt(k)) == UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS){
+									kanjiNum++;
+								}
+							}
+							long start = System.currentTimeMillis();
+							updateMessage(displayText);
+							int textLength = displayText.length();//LengthList.get(i);
+							int textScore = ScoreList.get(i);
+							System.out.println(displayText + " " + textLength + " " + textScore);
+							//int displayTime = (int)(baseTime + lengthWeight*textLength + scoreWeight*textScore);
+							displayTime = (int)(baseTime + a*(textLength-kanjiNum) + b*kanjiNum);
+							sumOfB++;
+							if(spaceBox.isSelected()==true){
+								while(true){
+									if(isNext){
+										isNext = false;
+										break;
+									}
+									Thread.sleep(1);
+								}
 
-						//計測したい処理を記述
+							}else{
+								try {
+									Thread.sleep(displayTime);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
+							}
 
-						long end = System.currentTimeMillis();
-						System.out.println((end - start)  + "ms");
-						String dicScore = "";
-						if(textScore<7&&textScore>0){
-							dicScore = String.valueOf(textScore);
+							//計測したい処理を記述
+
+							long end = System.currentTimeMillis();
+							System.out.println((end - start)  + "ms");
+							String dicScore = "";
+							if(textScore<7&&textScore>0){
+								dicScore = String.valueOf(textScore);
+							}
+							pw.println(end - start-1+","+textLength+","+LengthList.get(i)+","+kanjiNum+","+ textScore +","+ dicScore +","+displayText);
+							//System.out.println(sld.getValue());
 						}
-						pw.println(end - start-1+","+textLength+","+LengthList.get(i)+","+kanjiNum+","+ textScore +","+ dicScore +","+displayText);
-						//System.out.println(sld.getValue());
-					}
-					if(spaceBox.isSelected()){
-						pw.close();
+						if(spaceBox.isSelected()){
+							pw.close();
+						}
 					}
 					updateMessage("");
 					return null;
